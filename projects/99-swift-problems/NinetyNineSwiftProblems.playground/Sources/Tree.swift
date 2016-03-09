537ddeee1ed8b1da47d45e83da9d2002b6e02e8f
Eneko Alonso <eneko.alonso@gmail.com>
2016-03-08 22:23:43 -0800

    Tree and TreeSolutions

---
+++ projects/99-swift-problems/NinetyNineSwiftProblems.playground/Sources/Tree.swift
@@ -0,0 +1,26 @@
+
+public class Tree<T> {
+    var value: T
+    var left: Tree<T>?
+    var right: Tree<T>?
+
+    public init(_ value: T, _ left: Tree<T>? = nil, _ right: Tree<T>? = nil) {
+        self.value = value
+        self.left = left
+        self.right = right
+    }
+}
+
+extension Tree: CustomStringConvertible {
+    public var description: String {
+        if left != nil && right != nil {
+            return "T(\(value) \(left!) \(right!))"
+        } else if left != nil {
+            return "T(\(value) \(left!) nil)"
+        } else if right != nil {
+            return "T(\(value) nil \(right!))"
+        } else {
+            return "T(\(value) nil nil)"
+        }
+    }
+}
